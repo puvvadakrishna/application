649f35124433b691af2bdb46af261e056206f334
rama <puvvada.krishna@gmail.com>
2024-11-25 11:19:48 +0000

    rama| adding Junit for SecuredController

--- use/src/main/java/com/example/useful/OnlineShoppingValidator.java
+++ use/src/main/java/com/example/useful/OnlineShoppingValidator.java
@@ -1,16 +1,22 @@
 //Amazon
 //Ebay
-An add click generates a json request to your app. Based on the source value, the other 2 elements are optional or mandate
-        Provide a psudo code for the validate of optional attributes based on source
+An add click generates a json request to your app. Based on the source value, the other 2 elements are optional or mandate for different providers(Amazon/ebay)
+Provide a approach for the validate of optional attributes based on source
 
 {
         "source":"advert",
         "item":"iphone",
-        "model":"i16"
+        "model":"i16",
+        "discount":"10"
 }
 
-Hint to begin with:
+Hint to begin with:( consider scalablity/ & SOLID principles in the approach)
 
 public abstract class OnlineShoppingValidator<T extends Validator> {
 
 }
+
+
+
+
+
