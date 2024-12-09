79e774feeb9b605996182b58b0af6974c96d62b8
rama <puvvada.krishna@gmail.com>
2024-12-09 21:19:34 +0000

    rama | adding AT

--- app-shopping-acceptance-tests/src/test/java/com/shopping/CucumberSpringConfiguration.java
+++ app-shopping-acceptance-tests/src/test/java/com/shopping/CucumberSpringConfiguration.java
@@ -1,10 +1,8 @@
 package com.shopping;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.TestPropertySource;
 
 
 @CucumberContextConfiguration
