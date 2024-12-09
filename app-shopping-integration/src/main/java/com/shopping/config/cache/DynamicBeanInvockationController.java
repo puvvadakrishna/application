55e7130b67adb099c048315be97fbe1ef0093871
rama <puvvada.krishna@gmail.com>
2024-12-09 21:05:20 +0000

    rama| package refactoring

--- app-shopping-integration/src/main/java/com/shopping/config/cache/DynamicBeanInvockationController.java
+++ app-shopping-integration/src/main/java/com/shopping/config/cache/DynamicBeanInvockationController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/bean")
 public class DynamicBeanInvockationController {
